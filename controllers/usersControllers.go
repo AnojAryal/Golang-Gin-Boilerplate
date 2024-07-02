f876edd22e909c2ae5cfe3027e164b5ccbddd461
AnojAryal <anoj1810@gmail.com>
2024-07-02 08:56:42 +0545

    signup and login completed with jwttoken

--- controllers/usersControllers.go
+++ controllers/usersControllers.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/anojaryal/JWT-Authentication/initializers"
 	"github.com/anojaryal/JWT-Authentication/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -49,4 +52,73 @@ func SignUp(c *gin.Context) {
 
 	//respond
 	c.JSON(http.StatusCreated, gin.H{})
+}
+
+
+func Login(c *gin.Context) {
+	//get the email and pass of the req body
+	var body struct {
+		Email string
+		Password string
+	}
+
+	if c.Bind(&body) != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return 
+	}
+
+
+	//Look up requested user
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+
+		return 
+	}
+
+
+	//compare sent pass with saved user hash pass
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+
+		return
+	}
+
+
+	//generate jwt token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub" : user.ID,
+		"exp" : time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err:=  token.SignedString([]byte(os.Getenv("SECRET_KEY")))
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "fail to create token",
+		})
+
+		return
+	}
+
+
+
+	//sent it back
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("Authorization", tokenString, 3600 * 24 * 30, "", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{
+		// "token" : tokenString,
+	})
 }
